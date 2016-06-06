11c3b03053110ba865dced7fcd4368404fe0456e
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2016-06-06 17:02:55 -0300

    fixing config initializers

--- template_files/initializers/configs/cache.rb
+++ template_files/initializers/configs/cache.rb
@@ -1,14 +1,12 @@
-require_relative '../app_config'
-
 Rails.application.configure do
-  Config = Application::Config
+  _Config = Application::Config
 
-  cache_servers = (ENV["MEMCACHIER_SERVERS"]  || Config.cache_servers || "").split(",")
-  cache_username = ENV["MEMCACHIER_USERNAME"] || Config.cache_username
-  cache_password = ENV["MEMCACHIER_PASSWORD"] || Config.cache_password
+  cache_servers = (ENV["MEMCACHIER_SERVERS"]  || _Config.cache_servers || "").split(",")
+  cache_username = ENV["MEMCACHIER_USERNAME"] || _Config.cache_username
+  cache_password = ENV["MEMCACHIER_PASSWORD"] || _Config.cache_password
 
-  cache_namespace = Config.cache_namespace || "application-cache-#{Rails.env}"
-  cache_max_size  = (Config.cache_value_max_bytes || (1024 * 1024) * 10).to_f # 10 MB
+  cache_namespace = _Config.cache_namespace || "application-cache-#{Rails.env}"
+  cache_max_size  = (_Config.cache_value_max_bytes || (1024 * 1024) * 10).to_f # 10 MB
 
   cache_options = {
     :namespace            => cache_namespace,
@@ -16,14 +14,14 @@ Rails.application.configure do
     :username             => cache_username,
     :password             => cache_password,
     :failover             => true,
-    :socket_timeout       => (Config.cache_socket_timeout || 1.5).to_f,
-    :socket_failure_delay => (Config.cache_socket_failure_delay || 0.2).to_f,
+    :socket_timeout       => (_Config.cache_socket_timeout || 1.5).to_f,
+    :socket_failure_delay => (_Config.cache_socket_failure_delay || 0.2).to_f,
     # 10MB
     :value_max_bytes => cache_max_size
   }
 
   if (cache_servers.present? &&
-      (Config.cache_service == 'elastic_cache' && Config.enabled?(:cache_auto_discover_nodes)) &&
+      (_Config.cache_service == 'elastic_cache' && _Config.enabled?(:cache_auto_discover_nodes)) &&
       !!defined?(Dalli::ElastiCache))
     cache_endpoint = cache_servers.first
     # fetch all "disoverable" memcached nodes
--- template_files/initializers/configs/smtp.rb
+++ template_files/initializers/configs/smtp.rb
@@ -1,15 +1,13 @@
-require_relative '../app_config'
-
 Rails.application.configure do
-  Config = Application::Config
+  _Config = Application::Config
 
   config.action_mailer.delivery_method = :letter_opener
 
-  smtp_username = ENV['SENDGRID_USERNAME'] || Config.smtp_username
-  smtp_password = ENV['SENDGRID_PASSWORD'] || Config.smtp_password
-  smtp_host     = ENV['SENDGRID_HOST']     || Config.smtp_host
-  smtp_port     = ENV['SENDGRID_PORT']     || Config.smtp_port
-  smtp_domain   = ENV['SENDGRID_DOMAIN']   || Config.smtp_domain
+  smtp_username = ENV['SENDGRID_USERNAME'] || _Config.smtp_username
+  smtp_password = ENV['SENDGRID_PASSWORD'] || _Config.smtp_password
+  smtp_host     = ENV['SENDGRID_HOST']     || _Config.smtp_host
+  smtp_port     = ENV['SENDGRID_PORT']     || _Config.smtp_port
+  smtp_domain   = ENV['SENDGRID_DOMAIN']   || _Config.smtp_domain
 
   if [smtp_username, smtp_password, smtp_host, smtp_port, smtp_domain].all?
 
