fec404e1738513dbb29609d75c62e9f455be82b8
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2017-01-03 02:18:42 -0200

    Removing duplicate paginate helpers code

--- template_files/api/base.rb
+++ template_files/api/base.rb
@@ -1,8 +1,6 @@
 module API
   class Base < Grape::API
 
-    helpers API::Helpers::PaginateHelpers
-
     helpers API::Helpers::ApplicationHelpers
 
     prefix Application::Config.api_prefix_path if Application::Config.enabled?(:prefix_api_path)
